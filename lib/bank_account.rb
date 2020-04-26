4a408adac3b4d0fd55566e383690d0d471b78602
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-26 22:00:41 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
+n 
 class BankAccount
+  
+  attr_accessor :status, :balance
+  
+    def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = "open"
+    end
+    
+    def name
+      @name
+    end
+    
+    def deposit(addtlmoney)
+      newbalance = balance + addtlmoney
+      balance = newbalance
+      balance
+    end
 
+    def display_balance
+     puts "Your balance is $#{balance}."
+    end
+    
+    def valid 
+      if status == "open" && balance > 0 
+      end
+    end
+    
+    def close_account 
+      status = "closed"
+    end
+  
 end
