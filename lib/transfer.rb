9d52ffca01a0cd4c28f3d3980848b38fb2b773ca
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-27 01:56:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,16 @@ class Transfer
       end
     end
     
-    
+  #   def execute_transaction
+  #   if self.valid? && @sender.balance >= @amount && @status == "pending"
+  #     @sender.balance -= @amount
+  #     @receiver.balance += @amount
+  #     @status = "complete"
+  #   else
+		# @status = "rejected"
+		# "Transaction rejected. Please check your account balance."
+  #   end
+  # end
     
     def reverse_transfer
       if self.status = "complete"
