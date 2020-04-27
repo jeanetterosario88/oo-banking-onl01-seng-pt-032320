f991d66ff099601d5141acf096423f9fc39e76b0
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-27 01:54:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,8 +31,8 @@ class Transfer
     
     def reverse_transfer
       if self.status = "complete"
-        self.receiver.balance = self.receiver.balance + self.transferamount
-        self.sender.balance = self.sender.balance - self.transferamount
+        self.receiver.balance = self.receiver.balance + self.amount
+        self.sender.balance = self.sender.balance - self.amount
         self.status = "reversed"
       end
     end
