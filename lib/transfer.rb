c1f4a62d0111c667a8dd7ccfaf334a6058e1853e
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-27 01:58:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,25 +19,15 @@ class Transfer
     
     def execute_transaction
       if @sender.balance >= @amount && status = "pending"
-        self.receiver.balance = self.receiver.balance - self.amount
-        self.sender.balance = self.sender.balance + self.amount
-        self.status = "complete"
+        @receiver.balance = @receiver.balance - @amount
+        @sender.balance = @sender.balance @amount
+        @status = "complete"
       else 
-        status = "rejected"
+        @status = "rejected"
         return "Transaction rejected. Please check your account balance."
       end
     end
-    
-  #   def execute_transaction
-  #   if self.valid? && @sender.balance >= @amount && @status == "pending"
-  #     @sender.balance -= @amount
-  #     @receiver.balance += @amount
-  #     @status = "complete"
-  #   else
-		# @status = "rejected"
-		# "Transaction rejected. Please check your account balance."
-  #   end
-  # end
+
     
     def reverse_transfer
       if self.status = "complete"
