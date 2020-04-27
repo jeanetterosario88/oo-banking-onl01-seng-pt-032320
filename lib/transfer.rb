b18ba9572b86027c89efddccd74ca6ac1c6e8589
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-27 01:42:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,5 @@
 class Transfer
+  
  attr_accessor :sender, :reciever, :tranfer, :status
   
     def initialize(sender, reciever, transfer)
@@ -38,24 +39,3 @@ class Transfer
     
     
 end
-
-
-
-#   describe '#reverse_transfer' do
-#     it "can reverse a transfer between two accounts" do
-#       transfer.execute_transaction
-#       expect(amanda.balance).to eq(950)
-#       expect(avi.balance).to eq(1050)
-#       transfer.reverse_transfer
-#       expect(avi.balance).to eq(1000)
-#       expect(amanda.balance).to eq(1000)
-#       expect(transfer.status).to eq("reversed")
-#     end
-
-#     it "it can only reverse executed transfers" do
-#       transfer.reverse_transfer
-#       expect(amanda.balance).to eq(1000)
-#       expect(avi.balance).to eq(1000)
-#     end
-#   end
-# end
